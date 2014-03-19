273126d19f4dd3b729001eef1aa147ca246889d2
ForNein <ForNein@users.noreply.github.com>
2014-03-19 19:23:52 -0400

    New project
    Templates

---
+++ src/main/main.js
@@ -0,0 +1,14 @@
+function calculateScore(series) {
+	var score;
+	return score;
+}
+
+function handleStrike(series, index) {
+	var score;
+	return score;
+}
+
+function calculateScore(series, index) {
+	var score;
+	return score;
+}
---
+++ src/test/spec/mainSpec.js
@@ -0,0 +1,13 @@
+describe("Bowling Score Calculator", function() {
+	it("full game test", function() {
+
+	});
+
+	it("handle strike test", function() {
+
+	});
+
+	it("handle spare test", function() {
+
+	});
+});
